6a1e672f6f17454c33c5b8d662a02798411dd833
Akshay-Deshmane <akshaydeshmane1087@gmail.com>
2026-05-30 19:06:54 +0530

    Initial-CLI-Setup

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program.name("Intellex").description("Intellex").version("0.0.1");
+
+program.command("wakeup").description("show the banner and pick CLI or Telegram mode").action(
+    async()=>{
+        console.log("This is wakeup calling")
+    }
+);
+
+await program.parseAsync(process.argv);
