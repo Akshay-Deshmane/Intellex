5f5fbaed217f1f8ed577a3efee1da7e43d5c6a6d
Akshay-Deshmane <akshaydeshmane1087@gmail.com>
2026-06-10 09:00:35 +0530

    initial-agent-setup

---
+++ modes/agents/orchestrator.ts
@@ -0,0 +1,26 @@
+import { isCancel, text } from "@clack/prompts";
+import chalk from "chalk";
+import { defaultAgentConfig } from "./types";
+import { ActionTracker } from "./action-tracker";
+
+export async function runAgentMode() {
+ 
+    console.log(chalk.bold('\n Agent Mode \n'));
+
+    const goal = await text({
+        message : "what would you like to agent do ?",
+        placeholder : "Concrete task for this codebase..."
+    });
+
+    if(isCancel(goal) || !goal.trim()) {
+        return;
+    }
+
+    const config = defaultAgentConfig();  
+
+    const tracker = new ActionTracker();
+
+    
+
+    
+};
